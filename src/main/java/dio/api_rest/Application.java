ca11ce00627788294afe2d5f77932ff16ed75cee
Lidia Yamamura <108353487+RinoaYK@users.noreply.github.com>
2025-02-26 10:20:13 -0300

    Update Application.java

--- src/main/java/dio/api_rest/Application.java
+++ src/main/java/dio/api_rest/Application.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
-@OpenAPIDefinition(servers = {@Server(url = "postgresql://postgres:DURpVZDPoKNzUbggZjijyPQaEaeRTJBn@nozomi.proxy.rlwy.net:50825/railway", description = "Railway Server")})
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
